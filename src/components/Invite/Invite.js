e126582fe5a22813eea2648eebe3fa5bad444b2f
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-09-26 18:03:42 +0600

    authors name added to the list

--- src/components/Invite/Invite.js
+++ src/components/Invite/Invite.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Invite = ( props ) => {
-    const { inviteList } = props;
+    const { inviteList } = props || {};
 
     let totalCost = 0;
     for ( const guest of inviteList ) {
@@ -9,13 +9,18 @@ const Invite = ( props ) => {
     }
 
     return (
-        <div>
-            <h2 className="text-center fw-bold text-info">Invitation Summary</h2>
+        <div className="ps-3">
+            <h2 className="fw-bold text-info">Invitation Summary</h2>
             <hr className="text-secondary" />
-            <h4 className="text-center fw-bold text-info">Invited Authors: <span className="text-warning">{ inviteList.length }</span> </h4>
-            <h4 className="text-center fw-bold text-info">Total Fees: <span className="text-warning">{ totalCost }</span> </h4>
+            <h4 className="fw-bold text-info">Invited Authors: <span className="text-warning">{ inviteList.length }</span> </h4>
+            <h4 className="fw-bold text-info">Total Fees: <span className="text-warning">{ totalCost }</span> </h4>
             <hr className="text-secondary" />
-            <h4 className="text-center fw-bold text-info">Authors List:</h4>
+            <h4 className="fw-bold text-info">Authors List:</h4>
+            <ul>
+                {
+                    inviteList.map( invitee => <li className="fw-bold text-info">{ invitee.name }</li> )
+                }
+            </ul>
         </div>
     );
 };
